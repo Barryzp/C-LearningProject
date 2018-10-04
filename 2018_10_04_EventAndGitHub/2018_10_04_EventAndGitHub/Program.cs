1536cf0570a291e71c61b1ee9fe8887f863a869a
Barryzp <yihengzhang001@163.com>
2018-10-04 20:06:18 +0800

    C# Project in portablePC added.

---
+++ 2018_10_04_EventAndGitHub/2018_10_04_EventAndGitHub/Program.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _2018_10_04_EventAndGitHub
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("hello,sucker!");
+        }
+    }
+}

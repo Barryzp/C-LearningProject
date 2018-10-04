08b87767eb9ebd9d6ff6b3f5eeea85a506f33b37
Barryzp <yihengzhang001@163.com>
2018-10-04 22:35:08 +0800

    *Event learning files added.

---
+++ 2018_10_04_EventAndGitHub/2018_10_04_EventAndGitHub/EventTests/EventLearning.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _2018_10_04_EventAndGitHub
+{
+    public class Teacher
+    {
+        //claim a delegate.
+        public delegate void PrintWordsDelegate();
+        //claim a event.
+        public event PrintWordsDelegate SayingWords;
+
+        public void SpeakYourName()
+        {
+            Console.WriteLine("Guys,start to saying!");
+            SayingWords.Invoke();
+        }
+    }
+
+    public class Person
+    {
+        private string name;
+        public Person(string name)
+        {
+            this.name = name;
+        }
+
+        public virtual void Saying() { Console.WriteLine("My name is: " + name); }
+    }
+
+    public class Woman : Person
+    {
+        public Woman(string name) : base(name) { }
+    }
+    public class Man : Person
+    {
+        public Man(string name) : base(name) { }
+    }
+}
--- 2018_10_04_EventAndGitHub/2018_10_04_EventAndGitHub/Program.cs
+++ 2018_10_04_EventAndGitHub/2018_10_04_EventAndGitHub/Program.cs
@@ -8,9 +8,23 @@ namespace _2018_10_04_EventAndGitHub
 {
     class Program
     {
+        public static void EventTest()
+        {
+            Teacher teacher = new Teacher();
+            Woman mary = new Woman("Mary");
+            Man barry = new Man("Barry");
+
+            //To subscribe event that same params.
+            teacher.SayingWords += barry.Saying;
+            teacher.SayingWords += mary.Saying;
+
+            teacher.SpeakYourName();
+        }
+
         static void Main(string[] args)
         {
-            Console.WriteLine("hello,sucker!");
+            EventTest();
+            Console.WriteLine("end!");
         }
     }
 }

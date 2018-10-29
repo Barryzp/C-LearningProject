43f8a1717477697b2896d42dcef10bbfeace07c5
BarryPersonalPC <yihengzhang001@163.com>
2018-10-29 17:49:53 +0800

    Photon server step one,how to connect MySQL etc.

---
+++ 2018_10_29_PhotonServerLearning/2018_10_29_PhotonServerLearning/ConnectToMySQL.cs
@@ -0,0 +1,128 @@
+﻿using System;
+using MySql.Data.MySqlClient;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _2018_10_29_PhotonServerLearning
+{
+    class ConnectToMySQL
+    {
+        private string connectStr;
+        private MySqlConnection connection;
+        public ConnectToMySQL()
+        {
+            connectStr = "server=localhost;port=3306;database=mygamedb;user=root;password=Zp123456";
+            connection = new MySqlConnection(connectStr);
+        }
+
+        public void OpenDB()
+        {
+            //settle exception,cause we need know the reason why it crashed.
+            try
+            {
+                connection.Open();
+                Console.WriteLine("Connect successfully.");
+            }
+            catch (Exception e)
+            {
+                Console.WriteLine("Connect fail.\n"+e.ToString());
+                connection.Close();
+            }
+        }
+
+        public void CloseDB()
+        {
+            connection.Close();
+        }
+
+        public void Query(string sql) //how to query data from database;
+        {
+            MySqlCommand sqlCommand = new MySqlCommand(sql, connection);
+            MySqlDataReader reader = sqlCommand.ExecuteReader();
+            
+            while(reader.Read())//the return value means whether current page exists.(true/false)
+            {
+                Console.WriteLine(reader[0].ToString() + reader[1] + reader[2]);
+
+                //same function.
+                /*Console.WriteLine(reader.GetInt32(0)+" "+reader.GetString(1)+" "+reader.GetString(2));
+                Console.WriteLine(reader.GetInt32("id") + " " + reader.GetString("username") + " " + reader.GetString("password"));*/
+            }
+            reader.Close();
+        }
+
+        public object QueryToGetSingleValue(string sql)
+        {
+            MySqlCommand sqlCommand = new MySqlCommand(sql, connection);
+            return sqlCommand.ExecuteScalar();
+        }
+
+        public void Insert(string sql)
+        {
+            MySqlCommand sqlCommand = new MySqlCommand(sql, connection);
+            try
+            {
+                int result = sqlCommand.ExecuteNonQuery();
+                Console.WriteLine("insert success.");
+            }
+            catch (Exception e)
+            {
+                Console.WriteLine("insert fail.\n" + e.ToString());
+                connection.Close();
+            }
+        }
+
+        public void  Update(string sql)
+        {
+            MySqlCommand sqlCommand = new MySqlCommand(sql, connection);
+            try
+            {
+                int result = sqlCommand.ExecuteNonQuery();
+                Console.WriteLine("insert success.");
+            }
+            catch (Exception e)
+            {
+                Console.WriteLine("insert fail.\n" + e.ToString());
+                connection.Close();
+            }
+        }
+
+        public void Drop(string sql)
+        {
+            MySqlCommand sqlCommand = new MySqlCommand(sql, connection);
+            try
+            {
+                int result = sqlCommand.ExecuteNonQuery();
+                Console.WriteLine("insert success.");
+            }
+            catch (Exception e)
+            {
+                Console.WriteLine("insert fail.\n" + e.ToString());
+                connection.Close();
+            }
+        }
+
+        public bool VerifyUser(string username,string password)
+        {
+            //method 1;
+            //string sql = "select * from users where username='"+username+"' and password='"+password+"';";
+            string sql = "select * from users where username=@param1 and password=@param2;";
+            MySqlCommand sqlCommand = new MySqlCommand(sql, connection);
+            sqlCommand.Parameters.AddWithValue("param1", username);
+            sqlCommand.Parameters.AddWithValue("param2", password);
+            MySqlDataReader reader = sqlCommand.ExecuteReader();
+            
+            if (reader.Read())
+            {
+                //reader用完了记得关闭，不能够重复一直创建。
+                reader.Close();
+                return true;
+            }
+
+            reader.Close();
+            return false;
+        }
+    }
+}
---
+++ 2018_10_29_PhotonServerLearning/2018_10_29_PhotonServerLearning/Program.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _2018_10_29_PhotonServerLearning
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            ConnectToMySQL connect = new ConnectToMySQL();
+            connect.OpenDB();
+
+            connect.Query("select * from users;");
+            connect.Query("select * from users limit 2;");
+
+            /*var item = connect.QueryToGetSingleValue("select username from users where id=6;");
+            Console.WriteLine(item.ToString());*/
+
+            //connect.Insert("insert into users(username,password,registerdate) values('Slash','M123','"+DateTime.Now+"');");
+            //connect.Update("update users set username='DICKKKKKKK' where id=6;");
+            //connect.Drop("delete from users where id=4;");
+            //Console.WriteLine(connect.VerifyUser("barry", "123"));
+            //Console.WriteLine(connect.VerifyUser("jkkkkk", "123"));
+
+            connect.CloseDB();
+        }
+    }
+}

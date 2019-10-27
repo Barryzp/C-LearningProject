ae119f11a1b0b7bf68c7109971ff75ce68b772af
Barry_Desktop <yihengzhang001@163.com>
2019-10-27 19:09:57 +0800

    10.27.Server sync socket.

--- 2019_10_20_SocketServer/2019_10_20_SocketServer/Program.cs
+++ 2019_10_20_SocketServer/2019_10_20_SocketServer/Program.cs
@@ -10,7 +10,7 @@ namespace _2019_10_20_SocketServer
 {
     public class ClientState {
         public Socket socket;
-        public byte[] readBuff = new byte[1024];
+        public byte[] readBuff=new byte[1024];
     }
 
     class Program
@@ -19,7 +19,7 @@ namespace _2019_10_20_SocketServer
         static Socket listenfd;
         static void Main(string[] args)
         {
-            asyncServer();
+            syncServerImproving();
         }
 
         #region 异步服务器(无阻塞)
@@ -72,11 +72,12 @@ namespace _2019_10_20_SocketServer
                 }
 
                 string recvStr = System.Text.Encoding.UTF8.GetString(state.readBuff, 0, count);
-                string sendStr = "Server___"+recvStr;
+                string sendStr = clientfd.RemoteEndPoint.ToString()+":"+recvStr;
                 Console.WriteLine("服务器发送的数据："+sendStr);
-
                 byte[] sendClientBytes = System.Text.Encoding.UTF8.GetBytes(sendStr);
-                clientfd.Send(sendClientBytes);//不用异步，直接发
+                foreach (var skt in clients.Values) {
+                    skt.socket.Send(sendClientBytes);
+                }
                 clientfd.BeginReceive(state.readBuff, 0, 1024, 0, recvCallback, state);
             }
             catch (SocketException se) {
@@ -86,7 +87,6 @@ namespace _2019_10_20_SocketServer
         }
 
         #endregion
-
         #region 同步服务器(有阻塞)
         static void syncServer()
         {
@@ -118,5 +118,81 @@ namespace _2019_10_20_SocketServer
             }
         }
         #endregion
+
+        #region 同步服务器(改善1)
+        static void syncServerImproving()
+        {
+            Console.WriteLine("同步Server启动~");
+            Socket socket = new Socket(AddressFamily.InterNetwork, SocketType.Stream, ProtocolType.Tcp);
+
+            //绑定地址和端口号
+            IPAddress ipAddress = IPAddress.Parse("127.0.0.1");
+            IPEndPoint ipEndPoint = new IPEndPoint(ipAddress, 8888);//形成一个网络节点
+            socket.Bind(ipEndPoint);
+            socket.Listen(0);//表示指定队列中最多可接受的连接数，0代表不受限制
+            Console.WriteLine("同步Server启动成功~");
+            //开始监听来自客户端的请求
+            while (true)
+            {
+                if (socket == null) return;
+                if (socket.Poll(0, SelectMode.SelectRead)){
+                    readListenfd(socket);
+                }
+                foreach (var s in clients.Values){
+                    Socket clientfd = s.socket;
+                    if (clientfd.Poll(0, SelectMode.SelectRead)){
+                        if (!readClientfd(clientfd)) break;
+                    }
+                }
+                //防止死循环
+                System.Threading.Thread.Sleep(1);
+            }
+        }
+
+        //读取来自客户端的socket
+        public static void readListenfd(Socket listenfd) {
+            Console.WriteLine("Accept!");
+
+            Socket clientfd = listenfd.Accept();
+            ClientState state = new ClientState();
+            state.socket = clientfd;
+            clients.Add(clientfd, state);
+        }
+
+        public static bool readClientfd(Socket clientfd) {
+            ClientState state = clients[clientfd];
+
+            //处理接收
+            int count = 0;
+            try
+            {
+                count = clientfd.Receive(state.readBuff);
+            }
+            catch (SocketException se) {
+                clientfd.Close();
+                clients.Remove(clientfd);
+                Console.WriteLine("Socket recv failed!info: "+se.ToString());
+                return false;
+            }
+
+            //表示客户端关闭
+            if (count == 0) {
+                clientfd.Close();
+                clients.Remove(clientfd);
+                Console.WriteLine("Socket close." );
+                return false;
+            }
+            string recvStr = System.Text.Encoding.UTF8.GetString(state.readBuff,0,count);
+            Console.WriteLine("Recv "+recvStr);
+            string sendStr = clientfd.RemoteEndPoint.ToString() + recvStr;
+            byte[] sendBytes = System.Text.Encoding.UTF8.GetBytes(sendStr);
+
+            foreach (var skt in clients.Values) {
+                skt.socket.Send(sendBytes);
+            }
+            return true;
+        }
+        #endregion
+
     }
 }
